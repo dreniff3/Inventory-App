9741be01280b28143b332bd5ca597eb60d948ed7
Don Reniff <donaldreniff@gmail.com>
2024-09-16 14:36:35 -0700

    Add method for preprocessing image so easyocr can more accurately extract text

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import easyocr
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 import re
+
+
+def preprocess_image(img_path):
+    img = Image.open(img_path)
+
+    # Convert image to grayscale
+    img = img.convert('L')
+    # Increase contrast
+    enhancer = ImageEnhance.Contrast(img)
+    img = enhancer.enhance(2)
+    # Apply sharpening filter
+    img = img.filter(ImageFilter.SHARPEN)
+    # Resize to make text clearer
+    basewidth = 1000
+    wpercent = basewidth / float(img.size[0])
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.Resampling.LANCZOS)
+
+    # Save preprocessed image
+    img.save('preprocessed_image.jpeg')
+    return 'preprocessed_image.jpeg'
 
 
 def extract_text(img_path):
@@ -11,11 +32,12 @@ def extract_text(img_path):
     reader = easyocr.Reader(['en'])
 
     # Use EasyOCR to read text from image
-    result = reader.readtext(img_path)
+    result = reader.readtext(img_path, detail=0)
 
     # Extract and print text
     txt = ""
-    for (bbox, text, prob) in result:
+    # for (bbox, text, prob) in result:
+    for text in result:
         txt += f"{text}\n"
 
     return txt
@@ -42,5 +64,7 @@ def extract_titles(text):
 # The Catcher in the Rye"""
 # extract_titles(sample_text)
 
-extracted_text = extract_text('bookshelf.jpeg')
+# extracted_text = extract_text('bookshelf.jpeg')
+preprocessed_image = preprocess_image('dvdshelf.jpeg')
+extracted_text = extract_text(preprocessed_image)
 extract_titles(extracted_text)
